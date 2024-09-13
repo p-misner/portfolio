2ae47da4b7a9963e0f5981b18203712712107246
Priya Misner <priyamisner@priyas-mbp.myfiosgateway.com>
2024-09-12 23:10:45 -0400

    trying gh pages deploy again

--- next.config.js
+++ next.config.js
@@ -6,19 +6,6 @@ module.exports = {
    */
   output: "export",
 
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: "",
-
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
   images: {
     unoptimized: true,
   },
