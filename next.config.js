0488e163cd5987e1cd60be684b08d9bcbe183a1b
Priya Misner <priyamisner@priyas-mbp.myfiosgateway.com>
2024-09-12 20:54:28 -0400

    for github pages

--- next.config.js
+++ next.config.js
@@ -3,3 +3,11 @@ module.exports = {
     styledComponents: true,
   },
 };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/",
+  output: "export",
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
